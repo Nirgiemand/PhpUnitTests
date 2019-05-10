e3c19c5b69d59b264488773e943259cc98acab42
unknown <henri.tamvere@khk.ee>
2019-05-10 07:06:07 +0300

    02 Extend a unit test 01_07

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -1,19 +1,32 @@
 <?php
 namespace TDD\Test;
-require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
+# require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR .'autoload.php';
+require('vendor\autoload.php');
 
 use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
 // klass mis extendib TestCase klassi
 class ReceiptTest extends TestCase {
+
+	public function setup(){
+		// uus receipt objekti loomine
+		$this->Receipt = new Receipt();
+    }
+    // eemaldab loodud objekti receipt pärast testimist
+  public function teardown() {
+        unset($this->Receipt);
+    }
+	}
 	public function testTotal() {
-		// Uue funktsiooni ja objekti loomine
-		$Receipt = new Receipt();
+		// array vajalik meetodi kasutamiseks ja testimiseks
+		$input = [0, 2, 5, 8];
+		// Receipt meetodi kasutamine koos eelneva array-ga
+		$output = $this->Receipt->total($input);
 		$this->assertEquals(
-			// PHPUniti testimeetod, mis testib, kas oodatav väärtus on sama, mis assertEquals-is välja toodud
+			// Testib, kas oodatav väärtus on sama, mis assertEquals-is välja toodud
 			15,
-			$Receipt->total([0,2,5,8]),
+			$output,
 			'When summing the total should equal 15'
 		);
 	}
